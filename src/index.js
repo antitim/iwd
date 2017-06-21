27853a60e36f24672383565fb8eb578a8058ac83
unknown <antitim@yandex.ru>
2017-06-21 16:30:24 +0300

    fixed comparison

--- src/index.js
+++ src/index.js
@@ -3,27 +3,36 @@
  * @version 1.0.0
  * @author Maximilian Timofeev <antitim@yandex.ru>
  */
+'use strict';
 (function () {
   var js = function (target) {
-    var elements = target.querySelectorAll('[data-js]');
+    if (target.querySelectorAll) {
+      var elements = target.querySelectorAll('[data-js]');
 
-    for (var i = 0; i < elements.length; ++i) {
-      var item = elements[i];
-      var classes = item.classList;
-      classes.add('js');
+      for (var i = 0; i < elements.length; ++i) {
+        var item = elements[i];
+        var classes = item.classList;
+        classes.add('js');
 
-      if (!classes.contains('js_inited')) {
-        var name = item.getAttribute('data-js');
+        if (!classes.contains('js_inited')) {
+          var name = item.getAttribute('data-js');
 
-        if (typeof window[name] === 'function') {
-          window[name].call(item);
-          classes.add('js_inited');
+          if (typeof window[name] === 'function') {
+            window[name].call(item);
+            classes.add('js_inited');
+          }
         }
-      }
+      };
     };
   };
 
-  var Observer = MutationObserver || WebKitMutationObserver;
+  var Observer;
+
+  if (typeof MutationObserver !== 'undefined') {
+    Observer = MutationObserver;
+  } else if (typeof WebKitMutationObserver !== 'undefined') {
+    Observer = WebKitMutationObserver;
+  }
 
   if (Observer) {
     var observer = new Observer(function (mutations) {
