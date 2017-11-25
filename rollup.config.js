a4d0020271fa22dfcd222cf089caf3685a9d11ca
antitim <antitim@yandex.ru>
2017-11-25 05:36:49 +0300

    minify added

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
@@ -14,7 +13,6 @@ export default {
     babel({
       exclude: 'node_modules/**'
     }),
-    commonjs(),
-    //uglify()
+    uglify()
   ]
 };
